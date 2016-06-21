ccdf7b2fa2566ab93cfa4620daa0bff2e737be95
Merve <parin@Merves-MacBook-Pro.local>
2016-06-21 23:40:08 +0300

    Module13 examples added

---
+++ Module13/examples/WebSocket_Server/ChatServer.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Fleck.Handlers;
+using Fleck;
+namespace WebSocket_Server
+{
+    public class ChatServer
+    {
+        public void StartServer()
+        {
+            var allSockets = new List<IWebSocketConnection>();
+            var server = new WebSocketServer("ws://localhost:8181");
+            server.Start(socket =>
+            {
+                socket.OnOpen = () =>
+                {
+                    Console.WriteLine("Open!");
+                    allSockets.Add(socket);
+                };
+                socket.OnClose = () =>
+                {
+                    Console.WriteLine("Close!");
+                    allSockets.Remove(socket);
+                };
+                socket.OnMessage = message =>
+                {
+                    Console.WriteLine(message);
+                    allSockets.ToList().ForEach(s => s.Send("Server says: " + message));
+                };
+            });
+
+
+            var input = Console.ReadLine();
+            while (input != "exit")
+            {
+                foreach (var socket in allSockets.ToList())
+                {
+                    socket.Send(input);
+                }
+                input = Console.ReadLine();
+            }
+        }
+    }
+}
