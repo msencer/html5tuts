08dd6979fe8eb03adcd67396d0c405250867ddcb
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-06-21 15:06:36 +0300

    Module3 inclass updated

---
+++ Module3/examples/inclass/scripts/gamelogic.js
@@ -0,0 +1,47 @@
+﻿"use strict";
+var Game = function (seed) {
+    var score = 1000,
+        randomSayi = random(seed);
+
+    this.guess = function (userGuess) {
+        var guess =userGuess;
+        var diff = Math.abs(guess - randomSayi);
+        var feedback = {};
+
+        switch (diff) {
+            case 0:
+                feedback.status = true;      
+                break;
+            case 1:
+                score -= 20;
+                feedback.status = false;
+                break;
+            case 2:
+                score -= 50;
+                feedback.status = false;
+                break;
+            default:
+                score -= 100;
+                feedback.status = false;
+                break;
+        }
+        if (score <= 0) {
+            feedback.status = true;
+        }
+
+        feedback.score = score;
+
+        return feedback;
+    }
+
+    this.restart = function () {
+        score = 1000;
+        randomSayi = random(seed);
+    };
+
+    function random(seed) {
+        return Math.ceil(Math.random() * seed);
+    };
+
+    return this;
+};
---
+++ Module3/examples/inclass/scripts/jquery.game.js
@@ -0,0 +1,49 @@
+﻿$(function () {
+	
+	$.fn.guessgame = function (seed) {
+		var game = new Game(seed);
+
+		var txtGuess = $("<input>").attr("type", "text").attr("placeholder", "Guess a number");
+		var lblScore = $("<label>").text(1000);
+		var btnRestart = $("<button>").text("Restart");
+		var history = $("<ul>");
+
+
+		txtGuess.change(function () {
+			var tahmin = parseInt($(this).val());
+
+			var result = game.guess(tahmin);
+			var historyItem = $("<li>");
+			if (result.status) {
+				if (result.score <= 0)
+					historyItem.text("Oyun bitt!").addClass("incorrect");
+				else {
+					historyItem.text("Doğru tahmin").addClass("correct");
+					$(this).attr("disabled", true);
+				}
+			}
+			else {
+				historyItem.text("Yanlış tahmin!").addClass("incorrect");
+			}
+
+			history.append(historyItem);
+			lblScore.text(result.score);
+			$(this).val("");
+		});
+
+		btnRestart.click(function () {
+			game.restart();
+			txtGuess.prop("disabled", false);
+			history.empty();
+			lblScore.text("1000");
+		});
+
+		$(this).append(txtGuess);
+		$(this).append(lblScore);
+		$(this).append(btnRestart);
+		$(this).append(history);
+
+		txtGuess.focus();
+	}
+
+});
