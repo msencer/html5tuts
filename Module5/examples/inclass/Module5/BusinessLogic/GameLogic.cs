22cf367d2bdeeb35d7aff555d9d43045cf3ef57d
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-06-22 12:16:40 +0300

    Number guessing game logic added to inclass examples

---
+++ Module5/examples/inclass/Module5/BusinessLogic/GameLogic.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+
+namespace Module5.BusinessLogic
+{
+    public class GuessResult
+    {
+        public bool Status { get; set; }
+        public int Score { get; set; }
+    }
+    public class GameLogic
+    {
+        public int Seed { private get; set; }
+        private int RandomNumber {  get;  set; }
+        private int Score { get; set; }
+        public GameLogic(int seed)
+        {
+            Seed = seed;
+            Restart();
+        }
+
+        public GuessResult Guess(int guess)
+        {
+            var result = new GuessResult();
+            var diff = Math.Abs(RandomNumber - guess);
+
+            switch (diff)
+            {
+                case 0:
+                    result.Status = true;
+                    break;
+                case 1:
+                    result.Status = false;
+                    Score -= 20;
+                    break;
+                case 2:
+                    result.Status = false;
+                    Score -= 50;
+                    break;
+                default:
+                    result.Status = false;
+                    Score -= 100;
+                    break;
+            }
+
+            if(Score<=0)
+                result.Status = true;
+
+            result.Score = Score;
+
+            return result;
+        }
+
+        public void Restart()
+        {
+            Score = 1000;
+            RandomNumber = new Random().Next(1, Seed);
+        }
+    }
+}
---
+++ Module5/examples/inclass/Module5/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using Module5.BusinessLogic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Module5.Controllers
+{
+    public class GameController : Controller
+    {
+        // GET: Game
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult Guess(int guess)
+        {
+            if (Session["GameObject"] == null)
+                Session["GameObject"] = new GameLogic(10);
+
+            GameLogic logic = (GameLogic)Session["GameObject"];
+            
+            return Json(logic.Guess(guess), JsonRequestBehavior.AllowGet);
+        }
+
+    }
+}
