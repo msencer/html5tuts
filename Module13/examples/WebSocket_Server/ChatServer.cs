bdc0cfe7a94163065c7ff02d5aeb46a36e010822
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-06-22 16:15:23 +0300

    full chat example added

--- Module13/examples/WebSocket_Server/ChatServer.cs
+++ Module13/examples/WebSocket_Server/ChatServer.cs
@@ -5,43 +5,82 @@ using System.Text;
 using System.Threading.Tasks;
 using Fleck.Handlers;
 using Fleck;
+using System.Text.RegularExpressions;
+using System.Threading;
+
 namespace WebSocket_Server
 {
     public class ChatServer
     {
+        private readonly string IAM_REGEX = @"^IAM:(?<username>[a-zA-Z]{3,})$";
+        private readonly string MESSAGE_REGEX = @"^(?<username>[a-zA-Z]{3,})->(?<message>.+)$";
+        private Dictionary<string, IWebSocketConnection> allSockets = new Dictionary<string, IWebSocketConnection>();
         public void StartServer()
         {
-            var allSockets = new List<IWebSocketConnection>();
+            
             var server = new WebSocketServer("ws://localhost:8181");
+
             server.Start(socket =>
             {
                 socket.OnOpen = () =>
                 {
-                    Console.WriteLine("Open!");
-                    allSockets.Add(socket);
+                    Console.WriteLine("User connected!");
+                    socket.Send("WhoAreYou?");
                 };
                 socket.OnClose = () =>
                 {
-                    Console.WriteLine("Close!");
-                    allSockets.Remove(socket);
+                    Console.WriteLine("User disconnected!");
+                    foreach (var user in allSockets.Keys)
+                    {
+                        if (allSockets[user] == socket)
+                        {
+                            allSockets.Remove(user);
+                            return;
+                        }
+                    }
                 };
                 socket.OnMessage = message =>
                 {
-                    Console.WriteLine(message);
-                    allSockets.ToList().ForEach(s => s.Send("Server says: " + message));
+                    Console.WriteLine("Message received : {0}", message);
+                    Regex loginRE = new Regex(IAM_REGEX);
+
+                    var match = loginRE.Match(message);
+                    if (match.Length > 0)
+                    {
+                        string username = match.Groups["username"].Value.ToString();
+                        allSockets.Add(username, socket);
+                        Console.WriteLine("Username : {0}", username);
+                        socket.Send("WELCOME!");
+                        return;
+                    }
+                    Regex messageRE = new Regex(MESSAGE_REGEX);
+                    match = messageRE.Match(message);
+                    if (match.Length > 0)
+                    {
+                        string to = match.Groups["username"].Value;
+                        string msg = match.Groups["message"].Value;
+
+                        foreach(var user in allSockets.Keys)
+                        {
+                            if(allSockets[user] == socket)
+                            {
+                                Console.WriteLine("sending...");
+                                allSockets[to].Send(string.Format("{0}->{1}",user,msg));
+                                return;
+                            }
+                        }
+                    }
+
                 };
             });
 
-
-            var input = Console.ReadLine();
-            while (input != "exit")
-            {
-                foreach (var socket in allSockets.ToList())
-                {
-                    socket.Send(input);
-                }
-                input = Console.ReadLine();
-            }
+            Timer t = new Timer(SendUsers, null, 0, 1000);
+            Console.Read();
+        }
+        private void SendUsers(object o)
+        {
+            var allUserNames = "USERS:"+string.Join(";",allSockets.Keys);
+            allSockets.Values.ToList().ForEach(s => s.Send(allUserNames));
         }
     }
 }
