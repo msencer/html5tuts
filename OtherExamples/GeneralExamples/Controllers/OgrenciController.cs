4b0354224e2bcaec3d98e69194a9244e445627d8
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-06-23 12:45:19 +0300

    MVC unobtrusive example

---
+++ OtherExamples/GeneralExamples/Controllers/OgrenciController.cs
@@ -0,0 +1,30 @@
+﻿using GeneralExamples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralExamples.Controllers
+{
+    public class OgrenciController : Controller
+    {
+        // GET: Ogrenci
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Ogrenci obj)
+        {
+            if (ModelState.IsValid)
+            {
+                return Content("Kayıt eklendi");
+            }
+            
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ OtherExamples/GeneralExamples/Models/Ogrenci.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GeneralExamples.Models
+{
+    public class Ogrenci
+    {
+        [Required]
+        public string AdSoyad { get; set; }
+        [Range(1,4)]
+        public int Sinif { get; set; }
+        [Range(0, 4.0)]
+        public double Ortalama { get; set; }
+    }
+}
