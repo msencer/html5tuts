1a037464e492ca28f9e69fb387a8e0135df13603
Mehmet Sencer KARADAYI <msencerkaradayi@gmail.com>
2016-06-23 15:43:57 +0300

    YS example added

---
+++ OtherExamples/GeneralExamples/Controllers/YemekSepetiController.cs
@@ -0,0 +1,29 @@
+﻿using GeneralExamples.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GeneralExamples.Controllers
+{
+    public class YemekSepetiController : Controller
+    {
+        private IEnumerable<Urun> DummyUrunOlustur()
+        {
+            var r = new Random();
+            for (int i = 0; i < 10; i++)
+                yield return new Urun()
+                {
+                    Adet = r.Next(1, 100),
+                    Fiyat = (decimal)r.NextDouble() * 50,
+                    UrunAd = "Yemek " + r.Next(4, 44) + i
+                };
+        }
+        // GET: YemekSepeti
+        public ActionResult Index()
+        {
+            return View(model:DummyUrunOlustur());
+        }
+    }
+}
